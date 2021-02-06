77b558dc946f0dfeb20a4bc58a2733504cba0dfe
DiyaBaliga <baligadiya@gmail.com>
2021-02-06 11:37:15 -0800

    slight edits

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+//import { NavigationContainer, createBottomTabNavigator } from 'react-navigation';
+//import track from './components/track/track';
+import Plan from './components/plan/plan';
+
+
+//const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Plan/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
