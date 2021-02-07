d5c69cb861bdf496bd347706f05bebc54aaf3c70
Eva <evamfied@gmail.com>
2021-02-07 15:26:35 -0800

    minor changes

--- components/centre/MCentre.js
+++ components/centre/MCentre.js
@@ -21,7 +21,7 @@ export default function MCentre() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+
     alignItems: 'center',
     justifyContent: 'center',
   },
